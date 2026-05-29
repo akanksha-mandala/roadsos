0d91c90691441c93361edb6d499b7de58a2b8b81
Akanksha Mandala <akankshamandala7@gmail.com>
2026-05-30 05:08:15 +0530

    Final fix for deployment

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ def create_app():
         return render_template("index.html")
 
     return app
-
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True, port=5000)
